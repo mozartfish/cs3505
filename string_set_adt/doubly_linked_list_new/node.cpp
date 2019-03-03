152aec9bd2776f0152756e9a8c67ebf3f7b8c22b
Pranav Rajan <rajan.pranav320@gmail.com>
2019-03-03 13:58:22 -0700

    updated the node header file and the string_set.cpp file with the get_elements function

--- string_set_adt/doubly_linked_list_new/node.cpp
+++ string_set_adt/doubly_linked_list_new/node.cpp
@@ -21,12 +21,6 @@
  * node member function definitions
  ***************************************************** */
 
-/**
- * Initialize the constructor and destructor counters
- */
-long long cs3505::node::constructor_counter = 0;
-long long cs3505::node::destructor_counter = 0;
-
 /** Constructor:  Creates a node containing
   *   an element.  It is initialized to
   *   not point to any other node.
@@ -36,34 +30,21 @@ cs3505::node::node(const std::string & s)
     data(s)      // This calls the copy constructor - we are making a copy of the string.
 {
   // No other work needed - the initializers took care of everything.
-  cs3505::node::constructor_counter++;
 }
 
   
 /** Destructor:  release any memory allocated
   *   for this object.
   */
-
 cs3505::node::~node()
 {
   // I'm not convinced that the recursive delete is the
   //   best approach.  I'll keep it (and you'll keep it too).
 
-  cs3505::node::destructor_counter++;
   if (this->next != NULL)
     delete this->next;
 
   // Invalidate the entry so that it is not accidentally used.
 
   this->next = NULL;      
-}
-
-long long cs3505::node::get_constructor_count()
-{
-  return cs3505::node::constructor_counter;
-}
-
-long long cs3505::node:: get_destructor_count()
-{
-  return cs3505::node::destructor_counter;
 }
